c17e276eb55da7888c8f3fcd216bd5a87b784bc2
Ryan Allen <rallenwasd@gmail.com>
2020-03-23 11:50:02 -0600

    Annotate javascript/packs/readingList.jsx

--- app/javascript/packs/readingList.jsx
+++ app/javascript/packs/readingList.jsx
@@ -1,10 +1,16 @@
 import { h, render } from 'preact';
+// imports method that will generate/locate CSRF Token (cross-site forgery protection)
 import { getUserDataAndCsrfToken } from '../chat/util';
+// imports app/javascript/readingList/readingList.jsx
+// now all functionality from ReadingList is within this file's scope
 import { ReadingList } from '../readingList/readingList';
 
 function loadElement() {
   getUserDataAndCsrfToken().then(({ currentUser }) => {
+    // grabs reading-list from DOM
     const root = document.getElementById('reading-list');
+    // checks to make sure reading-list exists, then fills reading-list with
+    // data from ReadingList from '../readingList/readingList'
     if (root) {
       render(
         <ReadingList
@@ -18,6 +24,7 @@ function loadElement() {
   });
 }
 
+// run render everytime user clicks something (refreshes view with updates)
 window.InstantClick.on('change', () => {
   loadElement();
 });
