8c04e8469ae187a07ac894dc86fc17d56d991475
Ryan Allen <rallenwasd@gmail.com>
2020-03-26 08:05:10 -0600

    Refactor reading list items controller to retrieve all collections for current user and pass to erb

--- app/controllers/reading_list_items_controller.rb
+++ app/controllers/reading_list_items_controller.rb
@@ -3,7 +3,7 @@ class ReadingListItemsController < ApplicationController
     @reading_list_items_index = true
     set_view
     generate_algolia_search_key
-    @collections = ["A collection of cats", "A collection of rusty bicycles"]
+    @collections = current_user.collections
   end
 
   def update
