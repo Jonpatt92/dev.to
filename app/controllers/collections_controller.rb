80198cb8d9bc77c512c1269dab26a702e97bdfae
Ryan Allen <rallenwasd@gmail.com>
2020-03-28 15:53:59 -0600

    Add functionality to create new collections from readinglist articles. Create helper methods to retrieve articles from readinglist and assign to collection articles join table, then destroy readinglist articles from reactions table.
    Co-authored-by: Jonathan Patterson <jonpatt92@gmail.com>

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -5,7 +5,39 @@ class CollectionsController < ApplicationController
     @collection = User.find_by(username: params[:username]).collections.find_by(id: params[:id])
   end
 
+  def new; end
+
+  def create
+    new_collection = current_user.collections.new(collection_params)
+    if new_collection.save
+      assign_articles(new_collection.id)
+      redirect_to "/#{current_user.username}/collections/#{new_collection.id}"
+    else
+      flash.now[:global_notice] = "Title can't be blank"
+      render :new
+    end
+  end
+
   private
+
+  def reading_list_articles
+    current_user.reactions.where(category: "readinglist").pluck(:reactable_id)
+  end
+
+  def assign_articles(collection_id)
+    reading_list_articles.each do |article_id|
+      CollectionArticle.create(collection_id: collection_id, article_id: article_id) if article_id
+    end
+    destroy_reading_list
+  end
+
+  def destroy_reading_list
+    current_user.reactions.where(category: "readinglist").destroy_all
+  end
+
+  def collection_params
+    params.permit(:title, :description)
+  end
 
   def skip_bullet
     previous_value = Bullet.enable?
