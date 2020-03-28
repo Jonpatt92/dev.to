32e23600336c15d63b0b3930cd0edb8375fa5e20
Ryan Allen <rallenwasd@gmail.com>
2020-03-28 12:41:14 -0600

    Add initial test coverage for collections show page

---
+++ spec/system/user_views_a_collection_show_spec.rb
@@ -0,0 +1,26 @@
+require "rails_helper"
+
+RSpec.describe "Collections Show", type: :system do
+  let!(:user) { create(:user) }
+  let!(:collection) { create(:collection, user_id: user.id) }
+
+  before do
+    create(:article) # extra article for sad path
+    collection.articles << create_list(:article, 3)
+  end
+
+  context "when I visit a collections show page I see all articles listed that belong to the collection" do
+    it "Signed in user" do
+      sign_in user
+      visit "/#{user.username}/collections/#{collection.id}"
+      expect(page).to have_selector("#collections-show")
+      expect(page).to have_selector("#collections-article-item", count: 3)
+    end
+
+    it "Visitor" do
+      visit "/#{user.username}/collections/#{collection.id}"
+      expect(page).to have_selector("#collections-show")
+      expect(page).to have_selector("#collections-article-item", count: 3)
+    end
+  end
+end
