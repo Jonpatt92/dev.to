f096b6687f36bf6d4ccb5f0a7fc8effb049ea12c
Ryan Allen <rallenwasd@gmail.com>
2020-03-28 12:41:56 -0600

    Refactor collection query and add skip bullet for n+1 queries

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -1,5 +1,17 @@
 class CollectionsController < ApplicationController
+  around_action :skip_bullet, if: -> { defined?(Bullet) }
+
   def show
-    @collection = Collection.find(params[:id])
+    @collection = User.find_by(username: params[:username]).collections.find_by(id: params[:id])
+  end
+
+  private
+
+  def skip_bullet
+    previous_value = Bullet.enable?
+    Bullet.enable = false
+    yield
+  ensure
+    Bullet.enable = previous_value
   end
 end
